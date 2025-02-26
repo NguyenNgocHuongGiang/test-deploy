56f14ac059d7fd7fcfc559bd601b01a7fda3687a
S52100019 <52100019@student.tdtu.edu.vn>
2025-02-26 10:59:31 +0700

    update

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+// import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.scss";
+import "./index.css";
+import App from "./App.jsx";
+import "flowbite/dist/flowbite.min.js";
+import { store } from "./store";
+import { Provider } from "react-redux";
+
+createRoot(document.getElementById("root")).render(
+  // <StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
+  // </StrictMode>
+);
